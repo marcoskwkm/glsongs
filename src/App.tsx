85393d7075311a65cc785b1475d24c627988d99b
Marcos Kawakami <marcoskwkm@gmail.com>
2022-12-28 13:15:29 -0300

    Initialize repo

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => (
+  <div className="flex justify-center p-8">
+    <div className="flex w-full justify-center border-2 border-red-500 p-4">
+      Hello, world!
+    </div>
+  </div>
+)
+
+export default App
